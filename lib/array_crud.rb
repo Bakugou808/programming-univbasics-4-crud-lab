1f8659dd458a03008eaffe72325b6c40a807ce31
Janu Sung <janu.m.sung@gmail.com>
2019-12-11 03:47:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,42 +8,36 @@ end
 
 def add_element_to_end_of_array(array, element)
  
-  arr << 'okayyy'
+  arr << element 
   
 end
 
 def add_element_to_start_of_array(array, element)
-  arr = []
   
-  arr.unshift('hello world')
+  arr.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  arr = ['hi', 'hello', 'howdy']
   arr.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  arr = ['voo', 'le', 'vookooo', 'cest', 'vio']
   arr.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  arr = [1, 2, 3, 4]
   
-  arr[2]
+  arr[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  arr = [1, 2, 3, 4]
   arr[0]
 end
 
 def retrieve_last_element_from_array(array)
-  arr = [1,2,3,4,5]
   arr[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  arr[index_number] = element 
 end
